fb2c819fb66e5fe2f31138fdfa2043af35ed0cf6
Romain Fromi <romain.fromi@gmail.com>
2021-11-29 15:11:50 +0100

    Add public files (except the game box) and use the cover as the background image

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,4 @@
 /** @jsxImportSource @emotion/react */
-import { css, Global } from '@emotion/react'
 import GameState from '@gamepark/a-fistful-of-meeples/GameState'
 import {FailuresDialog, FullscreenDialog, Menu, useGame, usePlayer} from '@gamepark/react-client'
 import {Header, ImagesLoader, LoadingScreen} from '@gamepark/react-components'
@@ -9,9 +8,7 @@ import HTML5ToTouch from 'react-dnd-multi-backend/dist/cjs/HTML5toTouch'
 import PlayerColor from '../../rules/src/PlayerColor'
 import GameDisplay from './GameDisplay'
 import HeaderText from './HeaderText'
-import Cover from './images/Cover.jpg'
 import Images from './material/Images'
-import { letterBoxRatio } from './util/Metrics'
 
 export default function App() {
   const game = useGame<GameState>()
@@ -24,9 +21,8 @@ export default function App() {
   const loading = !game || imagesLoading || isJustDisplayed
   return (
     <DndProvider options={HTML5ToTouch}>
-      <Global styles={globalStyle} />
       {game && <GameDisplay game={game}/>}
-      <LoadingScreen display={loading} gameBox={Cover} author="Jonathan “Jonny Pac” Cantin" artist="Mihajlo Dimitrievski – The Mico" publisher="Final Frontier Games" developer="Jagrin" />
+      <LoadingScreen display={loading} author="Jonathan “Jonny Pac” Cantin" artist="Mihajlo Dimitrievski – The Mico" publisher="Final Frontier Games" developer="Jagrin" />
       <Header><HeaderText loading={loading} game={game} player={player?.id} /></Header>
       <Menu/>
       <FailuresDialog/>
@@ -35,50 +31,4 @@ export default function App() {
     </DndProvider>
   )
 }
-
-
-const globalStyle = css`
-  html {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-  }
-
-  *, *::before, *::after {
-    -webkit-box-sizing: inherit;
-    -moz-box-sizing: inherit;
-    box-sizing: inherit;
-  }
-
-  body {
-    margin: 0;
-    font-family: 'Oswald', "Roboto Light", serif;
-    font-size: 1vh;
-    @media (max-aspect-ratio: ${letterBoxRatio}) {
-      font-size: ${1 / letterBoxRatio}vw;
-    }
-  }
-
-  #root {
-    position: absolute;
-    height: 100vh;
-    width: 100vw;
-    user-select: none;
-    overflow: hidden;
-    background-color: white;
-    background-size: cover;
-    background-position: center;
-
-    &:before {
-      content: '';
-      display: block;
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.7);
-    }
-  }
-`
 
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import {css, Global} from '@emotion/react'
 import AFistfulOfMeeples from '@gamepark/a-fistful-of-meeples/AFistfulOfMeeples'
 import {AFistfulOfMeeplesOptionsDescription} from '@gamepark/a-fistful-of-meeples/AFistfulOfMeeplesOptions'
 import {GameProvider, setupTranslation} from '@gamepark/react-client'
@@ -8,14 +10,62 @@ import AFistfulOfMeeplesAnimations from './Animations'
 import App from './App'
 import translations from './translations.json'
 
-setupTranslation(translations, { debug: false })
+setupTranslation(translations, {debug: false})
+
+const style = css`
+  html {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  *, *::before, *::after {
+    -webkit-box-sizing: inherit;
+    -moz-box-sizing: inherit;
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    font-family: 'Oswald', "Roboto Light", serif;
+    font-size: 1vh;
+    @media (max-aspect-ratio: 185/100) {
+      font-size: ${100 / 185}vw;
+    }
+  }
+
+  #root {
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+    user-select: none;
+    overflow: hidden;
+    background-image: url(${process.env.PUBLIC_URL + '/cover-1920.jpg'});
+    background-color: white;
+    background-size: cover;
+    background-position: center;
+    color: #eee;
+
+    &:before {
+      content: '';
+      display: block;
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.8);
+    }
+  }
+`
 
 ReactDOM.render(
   <StrictMode>
     <GameProvider game="a-fistful-of-meeples" Rules={AFistfulOfMeeples} RulesView={AFistfulOfMeeplesView} optionsSpec={AFistfulOfMeeplesOptionsDescription}
-      animations={AFistfulOfMeeplesAnimations}    >
+                  animations={AFistfulOfMeeplesAnimations}>
       <App/>
     </GameProvider>
+    <Global styles={style}/>
   </StrictMode>,
   document.getElementById('root')
 )
